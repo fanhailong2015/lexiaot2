8437b32e6abfcb059773dd8f2bc8226f8cd8b2aa
duanzhiwei <89340545@qq.com>
2020-04-06 23:04:20 +0800

    'bak'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
         <div class="video-box">
-            <video class="video-box-video" src="https://v-cdn.zjol.com.cn/276982.mp4" controls></video>
-            <div class="video-box-controls"></div>
+            <video ref="video" @canplay="videoCanPlay()" @timeupdate="videoTimeUpdate()" class="video-box-video" src="https://v-cdn.zjol.com.cn/276982.mp4" poster="/static/logo.png" playsinline></video>
+            <div class="video-box-controls">
+                <div class="vcTop">
+                    <!-- 播放按钮 -->
+                    <div class="vcPlayBtn" @click="videoPlay()">{{vcIsPlay?'stop':'play'}}</div>
+                    <!-- 播放时间 -->
+                    <div class="vcPlayTime">{{vcCurrentTime}}/{{vcTotalTime}}</div>
+                    <!-- 全屏 -->
+                    <div class="vcFullBtn" @click="showFullScreen()">full</div>
+                </div>
+                <div class="vcBottom">
+                    <!-- 进度 -->
+                    <input type="range" @input="mySlidechange($event.target)" min="0" max="100" class="videoProgress" v-model="vcProgress" :style="{backgroundSize:+ vcProgress*100/100 +'% 100%'}"/>
+                </div>
+            </div>
         </div>
         <div class="center-box">
             <div></div>
@@ -18,6 +31,15 @@
     import Logo from '@/components/Logo'
     export default {
         name: 'Home',
+        data(){
+            return {
+                vcCurrentTime:'00:00',//当前进度
+                vcTotalTime:'00:00',//总时长
+                vcIsPlay:false,//video是否播放
+                vcProgress:0,//video进度
+                vcIsFull:false,//是否全屏
+            }
+        },
         components: {
             Logo
         },
@@ -25,7 +47,51 @@
 
         },
         methods: {
+            showFullScreen(){
+                // this.$refs.video.muted = !this.$refs.video.muted
+                // this.$refs.video.webkitRequestFullScreen();
+                if(this.$refs.video.requestFullScreen) {
+                    this.$refs.video.requestFullScreen();
+                } else if(this.$refs.video.mozRequestFullScreen) {
+                    this.$refs.video.mozRequestFullScreen();
+                } else if(this.$refs.video.webkitRequestFullScreen) {
+                    this.$refs.video.webkitRequestFullScreen();
+                }
+            },
+            onPlayerPlay() {
+                console.log('on player');
+            },
+            videoTimeUpdate(){
+                var currTime =this.$refs.video.currentTime;
+                var duration =this.$refs.video.duration;
+                this.vcCurrentTime = this.getFormatVideoTime(currTime);
+                var pre = currTime / duration;
+                this.vcProgress = pre*100;
+            },
+            videoCanPlay(){
+                var duration =this.$refs.video.duration;
+                // console.log('ss',duration)
+                this.vcTotalTime = this.getFormatVideoTime(duration);
+            },
+            //video play or stop
+            videoPlay(){
+                if(this.vcIsPlay){
+                    this.$refs.video.pause();
+                }else{
+                    this.$refs.video.play();
+                }
+                this.vcIsPlay = !this.vcIsPlay;
+            },
 
+            //格式化时间
+            getFormatVideoTime(time) {
+                var time = time;
+                var m = parseInt(time%3600/60),
+                    s = parseInt(time%60);
+                m = m < 10 ? "0"+m : m;
+                s = s < 10 ? "0"+s : s;
+                return m+":"+s;
+            }
         }
     }
 </script>
