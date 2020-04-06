38562ca4158ab4e81bf70c878a390bc79d408cc5
duanzhiwei <89340545@qq.com>
2020-04-06 20:48:33 +0800

    'bak'

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body,#app{
+    padding: 0;
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+  }
 </style>
