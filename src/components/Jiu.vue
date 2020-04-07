5252bd44d67d100a555910fe99957da83d3bc261
duanzhiwei <89340545@qq.com>
2020-04-07 20:45:10 +0800

    'bak'

---
+++ src/components/Jiu.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="blockDiv">
+        <div class="block">
+            <img src="/static/active1/1.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/2.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/3.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/4.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/5.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/6.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/7.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/8.gif"/>
+        </div>
+        <div class="block hui">
+            <img src="/static/active1/9.gif"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Jiu",
+        props: {
+            type: {
+                type: Number,
+                required: false,
+                default: 1
+            },
+            opend: {
+                type: Array,
+                required: false,
+                default: []
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .blockDiv{
+        width: 80%;
+        display:flex;
+        flex-wrap: wrap;
+        margin: 0 auto;
+    }
+    .block{
+        width: calc(calc(100% / 3) - 2px);
+        margin:1px;
+        box-sizing: border-box;
+    }
+    .block:hover{
+
+    }
+    .block img{
+        width: 100%;
+        display: block;
+    }
+    .hui{
+        filter: brightness(60%); /* W3C */
+    }
+</style>
--- src/views/Active.vue
+++ src/views/Active.vue
@@ -1,58 +1,19 @@
 <template>
-    <div class="blockDiv">
-        <div class="block">
-            <img src="/static/hd1/hd-1.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-2.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-3.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-4.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-5.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-6.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-7.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-8.png"/>
-        </div>
-        <div class="block">
-            <img src="/static/hd1/hd-9.png"/>
-        </div>
+    <div>
+        <jiu></jiu>
     </div>
 </template>
 
 <script>
+    import Jiu from '@/components/Jiu';
     export default {
-        name: "Active"
+        name: "Active",
+        components:{
+            Jiu
+        }
     }
 </script>
 
 <style scoped>
-    .blockDiv{
-        width: 80%;
-        display:flex;
-        flex-wrap: wrap;
-        margin: 0 auto;
-    }
-    .block{
-        width: calc(calc(100% / 3) - 2px);
-        margin:1px;
-        box-sizing: border-box;
-    }
-    .block:hover{
 
-    }
-    .block img{
-        width: 100%;
-        display: block;
-    }
 </style>
