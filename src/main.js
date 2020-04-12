322459b734ded7cb8458351722eaebb2044a6dfe
duanzhiwei <89340545@qq.com>
2020-04-12 19:04:16 +0800

    'bak'

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import './utils/axios'
 
 Vue.config.productionTip = false
 
+var _hmt = _hmt || [];
+window._hmt = _hmt; // 必须把_hmt挂载到window下，否则找不到（很多网友说需要添加我没加也成功了）
+(function () {
+    var hm = document.createElement("script")
+    hm.src = "https://hm.baidu.com/hm.js?66d1d50c210a9392bb9dc0ee01f3af5f"
+    var s = document.getElementsByTagName("script")[0]
+    s.parentNode.insertBefore(hm, s)
+})();
+
 new Vue({
     router,
     render: h => h(App),
